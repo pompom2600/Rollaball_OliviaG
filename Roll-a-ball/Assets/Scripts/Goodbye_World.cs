f399819cc1b1152d6950540629beed193da3659f
Olivia <ogonzalez1@myjmc.edu.au>
2021-02-26 09:57:08 +1100

    Add Base Unity Project
    -Added Project to Github
    -Simple player movement and Environment

---
+++ Roll-a-ball/Assets/Scripts/Goodbye_World.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goodbye_World : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int PlayerHealth = 100;
+    float PlayerTime = 0.10f;
+    string PlayerName = "BigBen205";
+    bool Dead = false;
+
+
+
+    void Start()
+    {
+   if (PlayerHealth > 0)
+        {
+            Debug.Log(Dead);
+
+        }
+        else
+        {
+
+            Debug.Log("Dead lmao");
+            Dead = true;
+
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void Potato()
+    {
+
+        Debug.Log("Fries or Chips?!");
+
+
+
+    }
+}
---
+++ Roll-a-ball/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody Rb;
+    public float jumpForce;
+    public float MoveSpeed = 2;
+
+
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+
+            Rb.AddForce(Vector3.up * jumpForce);
+             
+        }
+        float x = Input.GetAxis("Horizontal") * MoveSpeed;
+        float z = Input.GetAxis("Vertical") * MoveSpeed;
+        Rb.AddForce(x, 0, z);
+
+    }
+}
